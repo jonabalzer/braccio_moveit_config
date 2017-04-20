86a930052b901953ef1226308857fe03c27bccac
Jonathan Balzer <jonathan.balzer@vathos.net>
2017-04-20 14:03:20 +0200

    Added gripper.

--- arduino/braccio_driver.ino
+++ arduino/braccio_driver.ino
@@ -16,16 +16,26 @@ Servo gripper;
 
 #include <ros.h>
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Bool.h>
 
 ros::NodeHandle  nh;
 
 float RAD2DEG = 57.29577951308232;
 
 void servo_cb( const sensor_msgs::JointState& cmd_msg){
-  Braccio.ServoMovement(20, (int)(RAD2DEG*cmd_msg.position[0]),  (int)(RAD2DEG*cmd_msg.position[1]), (int)(RAD2DEG*cmd_msg.position[2]), (int)(RAD2DEG*cmd_msg.position[3]), (int)(RAD2DEG*cmd_msg.position[4]), 10);  
+  Braccio.ServoMovement(20, (int)(RAD2DEG*cmd_msg.position[0]),  (int)(RAD2DEG*cmd_msg.position[1]), (int)(RAD2DEG*cmd_msg.position[2]), (int)(RAD2DEG*cmd_msg.position[3]), (int)(RAD2DEG*cmd_msg.position[4]));  
 }
 
+void gripper_cb( const std_msgs::Bool& cmd_msg){
+  if((bool)cmd_msg.data)
+    Braccio.GripperMovement(20, 73);
+  else
+    Braccio.GripperMovement(20, 10);
+}
+
+
 ros::Subscriber<sensor_msgs::JointState> sub("joint_states", servo_cb);
+ros::Subscriber<std_msgs::Bool> sub_gripper("braccio_gripper", gripper_cb);
 
 void setup() {
 
@@ -36,6 +46,7 @@ void setup() {
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
+  nh.subscribe(sub_gripper);
 
 }
 
